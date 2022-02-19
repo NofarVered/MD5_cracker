07b22a137c99fe8306a860f23a03efc2a6d2f022
NofarVered <nofarss79@gmail.com>
2022-02-19 18:26:20 +0200

    changes in pps

--- master.py
+++ master.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-# Usage: python master.py -f ./test/hashes.txt -t 4
 
 import hashlib
 import argparse
 from popular_passwords_struct import popularPasswords
+import threading
+
+TOTAL_NUMBER = 100_000_000
 
 
 # Assumption: the number of popular passwords is less than the number of hashes passwords
@@ -28,6 +30,16 @@ def creat_output_file(founds):
     output_f.close
 
 
+# the tpl_rng is a tuple with start and end numbers for limit search
+def guess_numbers(start, end, hashes, founds):
+    for num in range(start, end+1):
+        current_guess = '05' + str(num).zfill(8)
+        enc_curr_guess = hashlib.md5(current_guess.encode("utf-8")).hexdigest()
+        if enc_curr_guess in hashes:
+            founds[enc_curr_guess] = current_guess
+            hashes.remove(enc_curr_guess)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -43,9 +55,25 @@ if __name__ == "__main__":
     open_read_hashes_file(file, hashes)
     print(popularPasswords)
     cheack_popular_passwords(hashes, founds)
-    # 1 --- create threads
-    # 2 --- do some shit inside the threads
+    # --- create threads
+    limit = int(TOTAL_NUMBER / threads_cnt)
+    range_pool = [(end - limit, end - 1)
+                  for end in range(limit, TOTAL_NUMBER+limit, limit)]
+    thread = []
+    try:
+        for i in range(threads_cnt):
+            run = threading.Thread(target=guess_numbers,
+                                   args=(range_pool[i][0], range_pool[i][1], founds))
+            run.start()
+            thread.append(run)
+        for j in thread:
+            j.join()
+    except:
+        # print "Erro"
+        pass
+
     # 3 --- @ Function definitions
     # 4 --- Read me file
+    # 5 --- TESTER
     creat_output_file(founds)
-    print("------------------ Your output file is ready in your folder... :-) ")
+    print("------------------ The output file is ready in your folder... :-) ")
--- test/file_generator.py
+++ test/file_generator.py
@@ -2,15 +2,13 @@ import hashlib
 import string
 import random
 
-f = open("./test/hashes.txt", mode="w")
-for i in range(0, 101):
-    num = '05'+''.join(random.choice(string.digits)
-                       for _ in range(8))
-    f.write(hashlib.md5(num.encode(
-        "utf-8")).hexdigest()+'\n')
 
-num = '05'+'00000000'
-f.write(hashlib.md5(num.encode("utf-8")).hexdigest()+'\n')
-
-print('------------------ Hashes file created, and ready for tests!')
-f.close
+def add_hashes_file():
+    f = open("./test/hashes.txt", mode="w")
+    for i in range(0, 101):
+        num = '05'+''.join(random.choice(string.digits)
+                           for _ in range(8))
+        f.write(hashlib.md5(num.encode(
+            "utf-8")).hexdigest()+'\n')
+    print('------------------ Hashes file created, and ready for tests!')
+    f.close
